fea5a19baa5379a262d2857aceb63a0daf24788b
Sylvia Lujo <thenightowl@Sylvias-MacBook-Pro.local>
2018-11-09 10:41:11 -0500

    Update: Fixed up manage users page. Admin can now edit and delete a user.

--- modules/users/client/controllers/admin/list-users.client.controller.js
+++ modules/users/client/controllers/admin/list-users.client.controller.js
@@ -5,13 +5,22 @@
     .module('users.admin')
     .controller('UserListController', UserListController);
 
-  UserListController.$inject = ['$scope', '$filter', 'AdminService'];
+  UserListController.$inject = ['$scope', '$filter', 'AdminService', 'Notification'];
 
-  function UserListController($scope, $filter, AdminService) {
+  function UserListController($scope, $filter, AdminService, Notification) {
     var vm = this;
     vm.buildPager = buildPager;
     vm.figureOutItemsToDisplay = figureOutItemsToDisplay;
     vm.pageChanged = pageChanged;
+
+    vm.displayUser = displayUser;
+    vm.selected_user = false;
+
+    vm.editUser = editUser;
+    vm.edit_user = false;
+
+    vm.remove = remove;
+    vm.update = update;
 
     AdminService.retrieveUsers().then(async function (data) {
 
@@ -45,6 +54,70 @@
 
     function pageChanged() {
       vm.figureOutItemsToDisplay();
+    }
+
+    function displayUser(user){
+      vm.user = user;
+      vm.user.edit_user = false;
+
+      console.log(vm.user);
+
+      vm.selected_user = true;
+      vm.user
+    }
+
+    function editUser(){
+      vm.user.edit_user = true;
+    }
+
+    function remove(user) {
+      if ($window.confirm('Are you sure you want to delete this user?')) {
+        if (user) {
+          user.$remove();
+
+          vm.users.splice(vm.users.indexOf(user), 1);
+          Notification.success('User deleted successfully!');
+        } else {
+          vm.user.$remove(function () {
+            $state.go('admin.users');
+            Notification.success({ message: '<i class="glyphicon glyphicon-ok"></i> User deleted successfully!' });
+          });
+        }
+      }
+    }
+
+    function update() {
+
+      var user = vm.user;
+      delete user.edit_user;
+
+      console.log("user: ",user);
+
+      console.log("update vm user: ",vm.user);
+      console.log(vm.user._id);
+
+
+      AdminService.updateUser(user._id, user).then(function(data){
+        console.log(data);
+        vm.user = user;
+        vm.user.edit_user = false;
+
+        /*$state.go('admin.user', {
+          userId: user._id
+        });*/
+        Notification.success({ message: '<i class="glyphicon glyphicon-ok"></i> User saved successfully!' });
+      }, function (errorResponse) {
+        Notification.error({ message: errorResponse.data.message, title: '<i class="glyphicon glyphicon-remove"></i> User update error!' });
+
+      });
+    /*  user.$update(function () {
+        $state.go('admin.user', {
+          userId: user._id
+        });
+        Notification.success({ message: '<i class="glyphicon glyphicon-ok"></i> User saved successfully!' });
+      }, function (errorResponse) {
+        Notification.error({ message: errorResponse.data.message, title: '<i class="glyphicon glyphicon-remove"></i> User update error!' });
+      });*/
     }
   }
 }());
