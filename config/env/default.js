511ab5a71ac1df489b47ffb7f5d63449d8faad71
Sylvia Lujo <thenightowl@Sylvias-MacBook-Pro.local>
2019-01-23 22:45:46 -0500

    Update: fixed app name

--- config/env/default.js
+++ config/env/default.js
@@ -2,9 +2,9 @@
 
 module.exports = {
   app: {
-    title: 'MEAN.JS',
-    description: 'Full-Stack JavaScript with MongoDB, Express, AngularJS, and Node.js',
-    keywords: 'mongodb, express, angularjs, node.js, mongoose, passport',
+    title: 'Careers Mentoring Program',
+    description: '',
+    keywords: '',
     googleAnalyticsTrackingID: process.env.GOOGLE_ANALYTICS_TRACKING_ID || 'GOOGLE_ANALYTICS_TRACKING_ID'
   },
   db: {
