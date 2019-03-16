b4173bd6b649fc8c7e6193a018106bd3a46da1c6
Sylvia Lujo <thenightowl@Sylvias-MacBook-Pro.local>
2019-03-16 01:49:33 -0400

    UPDATE:

--- modules/users/server/controllers/googleCloudStorage.server.controller.js
+++ modules/users/server/controllers/googleCloudStorage.server.controller.js
@@ -10,29 +10,28 @@ const bucketName = 'test-new-moto.appspot.com';
 
 const storage = new Storage({projectId});
 
-exports.uploadCloudFile = function(req, res) {
+exports.uploadCloudFile = async function(req, res) {
   console.log('yeEEEEEEEEE: ',req.body);
-  storage.bucket(bucketName).upload(`${req.body.name}`, {
+  await storage.bucket(bucketName).upload(`./uploads${req.body.name}`, {
     gzip: false,
     metadata: {
       cacheControl: 'no-cache',
     },
   });
 
-
   return res.status(200);
 };
 
 // Downloads the file
-exports.downloadCloudFile = function(req, res) {
+exports.downloadCloudFile = async function(req, res) {
   console.log('yeEEEE: ',req.params);
   const options = {
     destination: `./downloads/${req.params.filename}`,
   };
 
-  storage
+  await storage
     .bucket(bucketName)
-    .file(req.body.name)
+    .file(req.params.filename)
     .download(options);
 
   return res.status(200);
