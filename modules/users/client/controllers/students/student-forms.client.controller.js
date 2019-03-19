96cebfd194f3261f8af677197ec3918eac4a3273
Sylvia Lujo <thenightowl@Sylvias-MacBook-Pro.local>
2019-03-19 18:43:17 -0400

    Update

--- modules/users/client/controllers/students/student-forms.client.controller.js
+++ modules/users/client/controllers/students/student-forms.client.controller.js
@@ -156,7 +156,7 @@
       vm.credentials.ResumeId = `resume_${vm.credentials.username}.pdf`;
       $scope.uploading = true;
 
-      await uploadResumeToGoogleCloud($scope.file, vm.credentials.ResumeId);
+      uploadResumeToGoogleCloud($scope.file, vm.credentials.ResumeId);
     }
 
     async function uploadResumeToGoogleCloud(file, resumeId){
