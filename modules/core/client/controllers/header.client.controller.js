600ce7311578bf58340af1a1d291f8b059e668e7
Sylvia Lujo <thenightowl@Sylvias-MacBook-Pro.local>
2019-03-24 16:46:40 -0400

    Update: Added dashboards to home button

--- modules/core/client/controllers/header.client.controller.js
+++ modules/core/client/controllers/header.client.controller.js
@@ -14,12 +14,28 @@
     vm.authentication = Authentication;
     vm.isCollapsed = false;
     vm.menu = menuService.getMenu('topbar');
+    vm.dashboardOrHome = dashboardOrHome;
 
     $scope.$on('$stateChangeSuccess', stateChangeSuccess);
 
     function stateChangeSuccess() {
       // Collapsing the menu after navigation
       vm.isCollapsed = false;
+    }
+
+    function dashboardOrHome() {
+      if(vm.authentication.user === null){
+        $state.go('home', $state.previous.params);
+      }
+      else if(vm.authentication.user.roles.indexOf('admin') !== -1){
+        $state.go('admin.dash', $state.previous.params);
+      }
+      else if(vm.authentication.user.roles.indexOf('student') !== -1){
+        $state.go('student.application', $state.previous.params);
+      }
+      else if(vm.authentication.user.roles.indexOf('volunteer') !== -1){
+        $state.go('volunteer.application', $state.previous.params);
+      }
     }
   }
 }());
